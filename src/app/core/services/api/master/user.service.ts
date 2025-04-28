44f63b712d4b62b97a79fb30bd308077608e714d
sandeep <sandeep@ezatlas.com>
2025-04-29 01:23:59 +0530

    Committing user.service.ts and employee.component.ts

--- src/app/core/services/api/master/user.service.ts
+++ src/app/core/services/api/master/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+import { ApiService } from '../api.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService  {
 
-  constructor() { }
+  constructor(private apiSvc: ApiService) {
+    // super(apiSvc, "/api/companyDetails/Party")
+  }
+
+  postData(data: any): Observable<any> {
+    return this.apiSvc.post('/api/companyDetails/party', data);
+  }
+  getListViewData(): Observable<any> {
+    return this.apiSvc.get(`/api/masters/employee`)
+  }
+
 }
